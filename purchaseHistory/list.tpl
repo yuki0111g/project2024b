84acb99ff591abdb52ec5e10c370bd26f16e752a
yuki <yuki256f0111@gmail.com>
2024-06-21 13:47:17 +0900

    SQL更新に対応

--- purchaseHistory/list.tpl
+++ purchaseHistory/list.tpl
@@ -9,12 +9,13 @@
     <td>注文番号</td>
     <td>商品名</td>
     <td>1つあたりの価格</td>
-    <td>販売業者</td>
+    <td>ブランド</td>
     <td>注文日時</td>
     <td>商品ID</td>
     <td>個数</td>
     <td>合計金額</td>
     <td>ユーザID(テスト)</td>
+    <td>残りの在庫数(テスト)</td>
 
 </tr>
 
@@ -23,12 +24,13 @@
     <td> {$loop.orderId}</td>
     <td> {$loop.productName}</td>
     <td> {$loop.value}</td>
-    <td> {$loop.seller}</td>
+    <td> {$loop.brand}</td>
     <td> {$loop.timestamp}</td>
     <td> {$loop.productId}</td>
     <td> {$loop.num}</td>
     <td> {$loop.sumValue}</td>
     <td> {$loop.userId}</td>
+    <td> {$loop.stock}</td>
     </tr>
 {/foreach}
 
---
+++ sql/product.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2024-06-21 06:45:20
+-- サーバのバージョン： 10.4.32-MariaDB
+-- PHP のバージョン: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `2024b`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `product`
+--
+
+CREATE TABLE `product` (
+  `productId` int(11) NOT NULL,
+  `productName` varchar(255) NOT NULL,
+  `value` int(11) NOT NULL,
+  `isCool` tinyint(1) NOT NULL,
+  `expTime` date NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `product`
+--
+
+INSERT INTO `product` (`productId`, `productName`, `value`, `isCool`, `expTime`, `brand`, `amount`, `stock`) VALUES
+(1, 'りんご', 100, 0, '2024-06-27', 'スマイルshop', '100g', 0),
+(2, '牛肉', 2980, 1, '2024-06-13', 'スマイルshopUnofficial', '1000kg', 0),
+(3, 'さんま', 10000, 1, '2024-06-27', 'BottomValue', '', 0),
+(4, 'いちご', 100, 0, '2024-06-30', 'low損', '1t', 0),
+(5, '柔あげポテト', 198, 0, '2033-06-28', 'あいうえお', '72g', 0);
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productId`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `product`
+--
+ALTER TABLE `product`
+  MODIFY `productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

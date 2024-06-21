2ac127feef036a8b8ae220e556585475ac04d149
eno <c0a2202682@edu.teu.ac.jp>
2024-06-21 16:34:58 +0900

    カート

--- home_smtylist.php
+++ home_smtylist.php
@@ -1,25 +1,52 @@
 <?php
-$search = $_POST["input1"];
+session_start();
+$search = "";
+$oAmount = "";
+$ioAmount = 1;
+$wtb = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $search = $_POST["input1"];
+    $ioAmount = $_POST["orderAmount"];
+    $wtb = $_POST["wtb"];
+}
+if($ioAmount <= 0){
+    $ioAmount = 1;
+}
+$oAmount = strval($ioAmount);
+
+if(!isset($_SESSION["cart"])){
+    $_SESSION["cart"] = array();
+}
 
 require("libDB.php");
 $db = new libDB();
 $pdo = $db->getPDO();
-if($search == NULL){
-    $sqlMerc = $pdo->prepare("SELECT product_id, stock, productName FROM merchandise LEFT OUTER JOIN food ON merchandise.product_id = food.productId");
+
+
+if($search != ""){
+    $sql = $pdo->prepare("SELECT orderId, productName, value ,stock FROM order_history as oh LEFT OUTER JOIN product as p ON oh.productId = p.productId WHERE p.productName LIKE '{$search}%'");
+}
+elseif($wtb!= ""){
+    $sql = $pdo->prepare("SELECT productName, value FROM order_history as oh LEFT OUTER JOIN product as p ON oh.productID = p.productId WHERE oh.orderId = {$wtb}");
 }
 else{
-    $sqlMerc = $pdo->prepare("SELECT product_id, stock, productName FROM merchandise LEFT OUTER JOIN food ON merchandise.product_id = food.productId WHERE productName LIKE '{$search}'");
+    $sql = $pdo->prepare("SELECT orderId, productName, value ,stock FROM order_history as oh LEFT OUTER JOIN product as p ON oh.productId = p.productId");
 }
 //SQL文の実行
-$sqlMerc->execute();
+$sql->execute();
 //結果の取得
-$resultMarc = $sqlMerc->fetchAll();
-
-//Smartyのテンプレート設定
+$result = $sql->fetchAll();
 require_once("pnwsmarty.php");
-$pnwMerc = new pnwsmarty();
-$smartyMerc = $pnwMerc->getTpl();
-$smartyMerc->assign("resultMarc", $resultMarc);
-$smartyMerc->display("home/list.tpl");
-
+$pnw = new pnwsmarty();
+$smarty = $pnw->getTpl();
+if(($wtb != "")&&($search == "")){
+    $dump = array($result[0][0],$result[0][1],$oAmount);
+    array_push($_SESSION["cart"],$dump);
+    $smarty->assign("product",$_SESSION["cart"]);
+    $smarty->display("cart/cart.tpl");
+}
+else{
+    $smarty->assign("resultMarc", $result);
+    $smarty->display("home/list.tpl");
+}
 ?>
--- templates/home/list.tpl
+++ templates/home/list.tpl
@@ -1,29 +1,33 @@
-
 <html>  
 <head>
     <meta charset="UTF-8">
     <title>一覧</title>
 </head>      
 <body>
-<form action="home_smtylist.php" method="POST">
+<form "action="home_smtylist.php" method="POST">
 
 
 <h1>一覧</h1><table border="1">
-商品検索：<input type="text" size=20 maxlength="30" name="input1"><br>
+
+
 <tr>
+    <td>カートに追加</td>
     <td>name</td>
-    <td>stock</td>
     <td>price</td>
+    <td>stock</td>
 </tr>
-<form action="form2.php" method="POST">
 
 {foreach $resultMarc as $loop}
     <tr>
-    <td> {$loop.productName}</td>
+    <td> <input type="number" size=20 maxlength="30" name="orderAmount">
+    <input type="submit" name="wtb" value = {$loop.orderId}></td>
+    <td>{$loop.productName}</td>
+    <td> {$loop.value}</td>
     <td> {$loop.stock}</td>
     </tr>
 {/foreach}
-
+商品検索：<input type="text" size=20 maxlength="30" name="input1" value ""><br>
 </table>
+</form>
 </body>
 </html>

a7c7b75aa535c5717dd693deb2ccf7d4f7dfe6d1
eno <c0a2202682@edu.teu.ac.jp>
2024-06-28 16:13:25 +0900

    ファイル位置更新2

---
+++ productList.php
@@ -0,0 +1,23 @@
+<?php
+
+//商品一覧表示
+
+require("libDB.php");
+$db = new libDB();
+$pdo = $db->getPDO();
+
+//商品を検索せずに表示
+$sqlMerc = $pdo->prepare('SELECT * FROM product;');
+$sqlMerc->execute();
+$resultMarc = $sqlMerc->fetchAll();
+
+
+
+//Smartyのテンプレート設定
+require_once("pnwsmarty.php");
+$pnwMerc = new pnwsmarty();
+$smartyMerc = $pnwMerc->getTpl();
+$smartyMerc->assign("resultMarc", $resultMarc);
+$smartyMerc->display("productList/list.tpl");
+
+?>
---
+++ templates/productList/list.tpl
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./templates/productList/list.css">
+    <title>商品一覧</title>
+</head>
+
+<h1>商品一覧</h1><table border="1">
+<div id="column" class="column02">
+<h3>2個並び</h3>
+<ul>
+{foreach $resultMarc as $loop}
+<li><a href=""><img src={"./productImages/"|cat:$loop.image} /><p>{$loop.productName}</p><span>{$loop.value|cat:"円"}</span></a></li>
+{/foreach}
+</ul>
+</div>
+
+
+
+
